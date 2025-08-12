b3ba4ebcd590b4c490d03cd9106026f17a5b616d
swathigopavarapu <41499776+swathigopavarapu@users.noreply.github.com>
2025-08-12 11:23:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366; // Leap year
+  } else {
+    return 365; // Normal year
+  }
+}
+
+// Examples
+console.log(daysOfAYear(2022)); 
+console.log(daysOfAYear(2024));
+console.log(daysOfAYear(1900)); 
+console.log(daysOfAYear(2000)); 
+
